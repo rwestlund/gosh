c2f23f7e9d0acdc4edfd4ffeca302db667fb1293
Randy Westlund <rwestlun@gmail.com>
2019-01-18 17:26:20 -0800

    Abbreviate the path in the prompt

--- main.go
+++ main.go
@@ -44,7 +44,22 @@ func printPrompt(username, hostname string) {
 	if err != nil {
 		log.Println(errors.Wrap(err, "When getting cwd"))
 	}
+	cwd = abbreviatePath(cwd)
 	fmt.Fprintf(os.Stdout, "%s@%s %s> ", username, hostname, cwd)
+}
+
+// Convert e.g. /usr/home/foo/bar to /u/h/f/bar.
+func abbreviatePath(path string) string {
+	var parts = strings.Split(path, "/")
+	if len(parts) < 2 {
+		return path
+	}
+	for i := range parts[1 : len(parts)-1] {
+		if len(parts[i]) > 0 {
+			parts[i] = string(parts[i][0])
+		}
+	}
+	return strings.Join(parts, "/")
 }
 
 func handleLine(line string) {
